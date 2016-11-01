ed37a7bbb3370622c7088e8b9f977c256053b721
“kasper341” <kasper_341@hotmail.com>
2016-11-01 11:39:09 -0700

    add pop method

--- js/stack.js
+++ js/stack.js
@@ -1,6 +1,5 @@
 function Stack(size) {
 
-    console.log(size);
     this._list = [];
 
     this._size = size;
@@ -10,22 +9,30 @@ function Stack(size) {
 
 Stack.prototype.push = function (item) {
 
-    console.log(num);
-    // this.list.push(item);
     var index = this._index++;
 
     this._list[index] = item;
-    console.log(this._list);
-    // return size;
+    console.log("item pushed ", this._list);
+};
+
+Stack.prototype.pop = function () {
+
+    //remove last item
+    console.log(this._index);
+    var index = this._index--;
+    // delete this._list[index];
+    this._list.splice(index, 1);
+    console.log("item removed ", this._list);
 };
 
 
 var num = [77 ,99 ,44 ,55 ,22 ,88 ,11 ,0 ,66 ,33];
 var size = 5;
-// console.log(Stack(num));
+
 
 
 test = new Stack(size);
 
 test.push(20);
 test.push(30);
+test.pop();
