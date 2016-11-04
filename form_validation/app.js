758a25d2ab48349356ed4a39320e2491295b93cf
“kasper341” <kasper_341@hotmail.com>
2016-11-04 16:25:41 -0700

    update codility challenges

--- form_validation/app.js
+++ form_validation/app.js
@@ -2,6 +2,8 @@ $( document ).ready(function() {
 
     console.log( "ready!" );
 
+
+
     $("#type_person").click(function () {
 
         var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -12,20 +14,28 @@ $( document ).ready(function() {
         var lName = $("#last_name").val();
         var email = $("#email").val();
 
-        if (fName === "") {
+        if ((fName === "") || (lName === "")) {
             console.log("name in-valid");
-            return false;
-        }if (lName === ""){
-            console.log("name valid");
-            return false;
+            // return false;
+            result = false;
+            return result;
+        // }if (lName === ""){
+        //     console.log("name in-valid");
+        //     // return false;
+        //     result = false;
         }
         if (!re.test(email)) {
             console.log("email in-valid");
-            return false;
+            // return false;
+            result = false;
         }
         else{
-            return true;
+            // return true;
+            console.log("person test passed");
+            result = true;
         }
+
+        return result;
 
 
     });
@@ -34,18 +44,65 @@ $( document ).ready(function() {
 
         console.log("company clicked");
 
+        var result = false;
+
         var companyName = $("#company_name").val();
         var phone = $("#phone").val().replace(/[^A-Z0-9]/ig, "");
 
         if (companyName === "" || companyName === " ") {
-            return false;
+            console.log("in-valid company_name");
+            // return false;
+            result = false;
         }
         if (phone.length < 6) {
-            return false;
+            console.log("in-valid phone");
+            // return false;
+            result = false;
         }else{
-            return true;
+            console.log("company test passed");
+            // return true;
+            result = true;
         }
+
+        return result;
     });
+
+    // $("body").click(function () {
+    //     console.log("inside click event");
+    //
+    //     if (document.getElementById("type_person").checked === true) {
+    //
+    //         console.log("check person data");
+    //
+    //         var fName = $("#first_name").val();
+    //             var lName = $("#last_name").val();
+    //             var email = $("#email").val();
+    //
+    //             if ((fName === "") || (lName === "")) {
+    //                 console.log("name in-valid");
+    //                 // return false;
+    //                 result = false;
+    //                 return result;
+    //             // }if (lName === ""){
+    //             //     console.log("name in-valid");
+    //             //     // return false;
+    //             //     result = false;
+    //             }
+    //             if (!re.test(email)) {
+    //                 console.log("email in-valid");
+    //                 // return false;
+    //                 result = false;
+    //             }
+    //             else{
+    //                 // return true;
+    //                 console.log("person test passed");
+    //                 result = true;
+    //             }
+    //
+    //             return result;
+    //     }
+    //
+    // });
 
     // function validateForm() {
     //
--- js/codility_test1.js
+++ js/codility_test1.js
@@ -5,7 +5,7 @@ function solution(A, K) {
     var n = A.length;
     for (var i = 0; i < n - 1; i++) {
 //         if (A[i] + 1 != A[i + 1] && A[i] != 1)
-          if (A[i] + 1 != A[i + 1] && (A[i] != 1 && A[i] < K))
+          if ((A[i] + 1 > A[i + 1] && A[i] != 1 && A[i] !=K) || A[i]+1 < A[i+1])
             return false;
     }
      if ((A.length == K && A[n - 1] != K) || A.length < K)
@@ -14,9 +14,37 @@ function solution(A, K) {
         return true;
 }
 
-// var a = [1,2,2,3];
-var a = [1, 1, 2, 3, 4, 5, 5, 6, 8,8,9, 10];
-var k = 6;
+ //var a = [1, 1, 2, 3, 3];
+var a = [1, 1, 3];
+// var a = [1, 1, 2, 3, 4, 5, 5, 6, 8,8,9, 10];
+
+var k = 3;
 
 
 console.log(solution(a,k));
+
+
+// function solution(A, K) {
+//
+//     console.log(A);
+//
+//     var n = A.length;
+//     for (var i = 0; i < n - 1; i++) {
+// //         if (A[i] + 1 != A[i + 1] && A[i] != 1)
+//           if (A[i] + 1 != A[i + 1] && (A[i] != 1 && A[i] < K))
+//             return false;
+//     }
+//      if ((A.length == K && A[n - 1] != K) || A.length < K)
+//         return false;
+//     else
+//         return true;
+// }
+//
+// // var a = [1,2,2,3];
+// // var a = [1, 1, 3];
+// var a = [1, 1, 2, 3, 4, 5, 5, 6, 8,8,9, 10];
+//
+// var k = 5;
+//
+//
+// console.log(solution(a,k));
