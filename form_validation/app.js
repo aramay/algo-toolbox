602b47c856ee3bba2a77c54cf1e16fc642ea7425
“kasper341” <kasper_341@hotmail.com>
2016-11-03 19:24:57 -0700

    add phone validation

--- form_validation/app.js
+++ form_validation/app.js
@@ -1,48 +1,13 @@
 $( document ).ready(function() {
+
     console.log( "ready!" );
-
-
-    // var isChecked = $("#type_person").prop('checked');
-    // var x = document.getElementById("#type_person").checked;
-
 
     $("#type_person").click(function () {
 
         var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
         console.log("clicked");
 
-        var x = validateForm();
-        console.log(x);
-        // var fName = $("#first_name").val();
-        // var lName = $("#last_name").val();
-        // var email = $("#email").val();
-        //
-        // if (fName === "") {
-        //     console.log("name in-valid");
-        //     return false;
-        // }if (lName === ""){
-        //     console.log("name valid");
-        //     return false;
-        // }
-        // if (!re.test(email)) {
-        //     console.log("email in-valid");
-        //     return false;
-        // }
-        // else{
-        //     return true;
-        // }
-
-
-    });
-
-    function validateForm() {
-
-        console.log("validate form");
-
-        var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        console.log("clicked");
-
-        // var x = validateForm();
         var fName = $("#first_name").val();
         var lName = $("#last_name").val();
         var email = $("#email").val();
@@ -61,12 +26,54 @@ $( document ).ready(function() {
         else{
             return true;
         }
-    }
 
-console.log("test");
-    if (document.getElementById("type_person").checked === true) {
-        console.log("form validation");
-    }
+
+    });
+
+    $("#type_company").click(function () {
+
+        console.log("company clicked");
+
+        var companyName = $("#company_name").val();
+        var phone = $("#phone").val().replace(/[^A-Z0-9]/ig, "");
+
+        if (companyName === "" || companyName === " ") {
+            return false;
+        }
+        if (phone.length < 6) {
+            return false;
+        }else{
+            return true;
+        }
+    });
+
+    // function validateForm() {
+    //
+    //     console.log("validate form");
+    //
+    //     var re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    //     console.log("clicked");
+    //
+    //     // var x = validateForm();
+    //     var fName = $("#first_name").val();
+    //     var lName = $("#last_name").val();
+    //     var email = $("#email").val();
+    //
+    //     if (fName === "") {
+    //         console.log("name in-valid");
+    //         return false;
+    //     }if (lName === ""){
+    //         console.log("name valid");
+    //         return false;
+    //     }
+    //     if (!re.test(email)) {
+    //         console.log("email in-valid");
+    //         return false;
+    //     }
+    //     else{
+    //         return true;
+    //     }
+    // }
 
 
 });
