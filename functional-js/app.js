eab68bbe154c77ff753f52d97cbe30ae82856906
“kasper341” <kasper_341@hotmail.com>
2016-11-06 16:48:02 -0800

    add functional prog example

--- functional-js/app.js
+++ functional-js/app.js
@@ -1,12 +1,59 @@
-<!DOCTYPE html>
-<html>
-<body>
+var test = function() {
+	var newReleases = [
+		{
+			"id": 70111470,
+			"title": "Die Hard",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [4.0],
+			"bookmark": []
+		},
+		{
+			"id": 654356453,
+			"title": "Bad Boys",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [5.0],
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		},
+		{
+			"id": 65432445,
+			"title": "The Chamber",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [4.0],
+			"bookmark": []
+		},
+		{
+			"id": 675465,
+			"title": "Fracture",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": [5.0],
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		}
+	],
+	videoAndTitlePairs = [];
 
-<p id="demo"></p>
+	// ------------ INSERT CODE HERE! -----------------------------------
+	// Use forEach function to accumulate {id, title} pairs from each video.
+	// Put the results into the videoAndTitlePairs array using the Array's
+	// push() method. Example: videoAndTitlePairs.push(newItem);
+	// ------------ INSERT CODE HERE! -----------------------------------
+  var pair = {};
+
+  newReleases.forEach(function(video){
+
+    // console.log(video);
+    pair.id = video.id;
+    pair.title = video.title;
+
+    // console.log(pair);
+    videoAndTitlePairs.push(pair);
+  });
 
 
-<!-- <script src="insertion_sort.js"> -->
-<script src="codility_test1.js"></script>
+	return videoAndTitlePairs;
+}
 
-</body>
-</html>
+console.log(test());
