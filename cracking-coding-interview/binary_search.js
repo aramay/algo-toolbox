c1f51559a4b0f83e242fea7016d87368bf400428
“kasper341” <kasper_341@hotmail.com>
2016-11-10 14:16:45 -0800

    finish binary search

--- cracking-coding-interview/binary_search.js
+++ cracking-coding-interview/binary_search.js
@@ -1,5 +1,28 @@
-function binarySearch(N) {
+function binarySearch(N, num) {
     console.log(N);
+
+
+    var middle = N.length / 2;
+
+    middle = Math.ceil(middle);
+
+    if (num === N[middle]) {
+        return num;
+    }
+    if (N.length === 1) {
+        return num;
+    }
+
+    if (num > N[middle]) {
+
+        return this.binarySearch(N.splice(middle), num);
+    }
+
+    else{
+        return this.binarySearch(N.slice(0,middle), num);
+    }
+
+
 }
 
-console.log(binarySearch([12,54,34,6,7,89]));
+console.log(binarySearch([10,20,30,40,50,60,70], 10));
