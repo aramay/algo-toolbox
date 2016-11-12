9a41cd4419ca3987efbc120fb9cbff11c807eb5d
“kasper341” <kasper_341@hotmail.com>
2016-11-11 21:25:48 -0800

    finish addition

--- functional-js/ex2/ex2.js
+++ functional-js/ex2/ex2.js
@@ -1,6 +1,16 @@
-function foo() { /* .. */ }
+function foo(x, y) {
+
+    return function(){
+        console.log("test");
+
+        return x + y;
+    };
+
+}
 
 var x = foo(3,4);
 
-x();	// 7
+console.log( x() );	// 7
+console.log( x() );	// 7
+
 x();	// 7
