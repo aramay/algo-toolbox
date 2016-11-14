3b5dcec41953c7f13677e2beeeb372c68ca8e04a
“kasper341” <kasper_341@hotmail.com>
2016-11-14 15:22:05 -0800

    pending string compression

---
+++ cracking-coding-interview/string_compress.js
@@ -0,0 +1,17 @@
+function stringCompression(N) {
+
+    console.log(N);
+
+    for (var i = 0; i < N.length; i++) {
+//         console.log(N[i]);
+
+        if (N[i] === " ") {
+            // N[i] = "%20";
+            
+        }
+    }
+
+    console.log(N);
+}
+
+console.log(stringCompression("Mr John Smith"));
