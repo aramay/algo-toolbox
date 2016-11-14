7668c07f9d2bf377153535ca5b81aa86594e7a6e
“kasper341” <kasper_341@hotmail.com>
2016-11-14 10:26:16 -0800

    add nested filter and map methods

---
+++ functional-js/map-implement/filter-map.js
@@ -0,0 +1,54 @@
+function test() {
+	var newReleases = [
+		{
+			"id": 70111470,
+			"title": "Die Hard",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 654356453,
+			"title": "Bad Boys",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		},
+		{
+			"id": 65432445,
+			"title": "The Chamber",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 675465,
+			"title": "Fracture",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		}
+	];
+
+	// ------------ INSERT CODE HERE! -----------------------------------
+	// Chain the filter and map functions to select the id of all videos
+	// with a rating of 5.0.
+
+
+
+	 newReleases.filter(function (video) {
+
+         console.log(video);
+         return video.rating === 5.0;
+     }).map(function(video){
+         return video.id;
+     });
+    // Complete this expression
+	// ------------ INSERT CODE HERE! -----------------------------------
+}
+
+test();
