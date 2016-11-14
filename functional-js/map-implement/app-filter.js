f1b97d7e59efd7c805a9d37607cdf34461851626
“kasper341” <kasper_341@hotmail.com>
2016-11-14 10:25:29 -0800

    add filter function

---
+++ functional-js/map-implement/app-filter.js
@@ -0,0 +1,65 @@
+function() {
+	var newReleases = [
+		{
+			"id": 70111470,
+			"title": "Die Hard",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 654356453,
+			"title": "Bad Boys",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		},
+		{
+			"id": 65432445,
+			"title": "The Chamber",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 675465,
+			"title": "Fracture",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		}
+	],
+
+	videos = [];
+
+	// ------------ INSERT CODE HERE! -----------------------------------
+	// Use forEach function to accumulate every video with a rating of 5.0
+	// ------------ INSERT CODE HERE! -----------------------------------
+	newReleases.forEach(function(video){
+
+      if(video.rating === 5.0){
+
+        videos.push(video);
+      }
+
+
+	});
+	return videos;
+}
+
+Array.prototype.filter = function(predicateFunction) {
+	var results = [];
+	this.forEach(function(itemInArray) {
+	  if (predicateFunction(itemInArray)) {
+		results.push(itemInArray);
+	  }
+	});
+
+	return results;
+};
+
+// JSON.stringify([1,2,3].filter(function(x) { return x > 2})) === "[3]"
