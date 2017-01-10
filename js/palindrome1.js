7764ca58a706f68ae11147abd6f10da84842263f
“kasper341” <kasper_341@hotmail.com>
2017-01-10 12:57:44 -0800

    add 'regex' to detect non-aplhanumeric chars

--- js/palindrome1.js
+++ js/palindrome1.js
@@ -1,18 +1,39 @@
-var isPalindrome = function (input) {
+// var isPalindrome = function (input) {
 
-    var i = 0;
-    var x = input.length - 1;
+//     var i = 0;
+//     var x = input.length - 1;
 
-    while (i < x) {
+//     while (i < x) {
 
-        if (input[i] == input[x]) {
-            i ++;
-            x --;
-        } else {
-            return false;
-        }
+//         if (input[i] == input[x]) {
+//             i ++;
+//             x --;
+//         } else {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
+function palindrome(str) {
+  // Good luck!
+  var x = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  
+  var start = 0;
+  var end = x.length - 1;
+  
+  while(start < end){
+    if(x[start] === x[end]){
+      
+      start ++;
+      end --;
+      
+    }else{
+      return false;
     }
-    return true;
-};
 
-console.log(isPalindrome("mooom"));
+  }
+  return true;
+}
+
+console.log(palindrome("race car"));
