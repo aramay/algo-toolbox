aecf03183b8dab0c00bafbc18e74b9738269c2a5
“kasper341” <kasper_341@hotmail.com>
2017-01-13 11:19:30 -0800

    finish fibonacci top-down approach

--- js/fibonacci-top-down.js
+++ js/fibonacci-top-down.js
@@ -1,12 +1,32 @@
-function fibonacciTopDown(){
+function fibonacciTopDown(n){
 
-    
+    var result = [];
 
-    function computeFibonacci(){
+    function computeFibonacci(num){
+
+// console.log(num)
+
+        if(num < 2){ 
+            return num
+        }else{
+
+            result.push(
+                computeFibonacci(num - 2) + 
+                computeFibonacci(num - 1)
+                )
+            // return computeFibonacci(num - 2) + computeFibonacci(num - 1)
+        }
+
+        // console.log(computeFibonacci(num - 2) + computeFibonacci(num - 1))
+
+        // computeFibonacci(num) // recursive call, this is stupid, AGAIN
 
     }
+
+    // result = computeFibonacci(n)//invoke helper
+    result.push(computeFibonacci(n))
 
     return result;
 }
 
-consolelog(fibonacciTopDown(10))
+console.log(fibonacciTopDown(10))
