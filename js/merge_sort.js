fb1c51ce332065859838788f893a73642fcdbf6b
“kasper341” <kasper_341@hotmail.com>
2017-01-13 10:49:37 -0800

    refactored code to return results

--- js/merge_sort.js
+++ js/merge_sort.js
@@ -1,88 +1,102 @@
 //3rd try
 
-function mergeSort(arr){
-
-    if(arr.length < 2){
-        return arr;
-    }
-
-    var midIndex = Math.floor(arr.length / 2),
-        left = arr.slice(0,midIndex),
-        right = arr.slice(midIndex, arr.length )
-
-    return merge(mergeSort(left), (right))
-}
-
-function merge(left, right){
-    var result = [],
-        index1 = 0,
-        index2 = 0;
-    
-    while(index1 < left.length && index2 < right.length){
-
-        if(left[index1] <= right[index2]){
-
-            result.push(left[index1]);
-            index1 ++;
-        }else{
-            result.push(right[index2]);
-            index2 ++;
-        }
-    }
-    if (index1 === left.length){
-        while (index2 < right.length){
-        result.push(right[index2]);
-        index2++;
-        }
-    } else {
-        while (index1 < left.length){
-        result.push(left[index1]);
-        index1++;
-        }
-  }
-
-//   return result;
-
-    console.log(result);
-}
-
-//1st try
 // function mergeSort(arr){
-    
-//     //base case
+
 //     if(arr.length < 2){
 //         return arr;
 //     }
 
-//     var midIndex = Math.floor(arr.length / 2)
-//     var left = arr.slice(0,midIndex)
-//     var right = arr.slice(midIndex, arr.length )
+//     var midIndex = Math.floor(arr.length / 2),
+//         left = arr.slice(0,midIndex),
+//         right = arr.slice(midIndex, arr.length )
 
-//     console.log("sortedLeft before recursive call", sortedLeft)
-//     console.log("sortedRight before recursive call", sortedRight)
+//     return merge(mergeSort(left), (right))
+// }
 
-//     var sortedLeft = mergeSort(left)
-//     var sortedRight = mergeSort(right)
-
-//     console.log("sortedLeft after recursive call", sortedLeft)
-//     console.log("sortedRight after recursive call", sortedRight)
-
+// function merge(left, right){
 //     var result = [],
 //         index1 = 0,
 //         index2 = 0;
+    
+//     while(index1 < left.length && index2 < right.length){
 
-//     while(index1 < sortedLeft.length && index2 < sortedRight.length){
-//         if(sortedLeft[index1] <= sortedRight[index2]){
-//             result.push(sortedLeft[index1]);
+//         if(left[index1] <= right[index2]){
 
-//             index1 ++
+//             result.push(left[index1]);
+//             index1 ++;
 //         }else{
-//             result.push(sortedRight[index2]);
-
-//             index2 ++
+//             result.push(right[index2]);
+//             index2 ++;
 //         }
 //     }
+//     if (index1 === left.length){
+//         while (index2 < right.length){
+//         result.push(right[index2]);
+//         index2++;
+//         }
+//     } else {
+//         while (index1 < left.length){
+//         result.push(left[index1]);
+//         index1++;
+//         }
+//   }
+
+// //   return result;
+
+//     console.log(result);
 // }
+
+//1st try
+function mergeSort(arr){
+    
+    //base case
+    if(arr.length < 2){
+        return arr;
+    }
+
+    var midIndex = Math.floor(arr.length / 2)
+    var left = arr.slice(0,midIndex)
+    var right = arr.slice(midIndex, arr.length )
+
+    // console.log("sortedLeft before recursive call", sortedLeft)
+    // console.log("sortedRight before recursive call", sortedRight)
+
+    var sortedLeft = mergeSort(left) // this splits array into half untill two or smaller elements
+    var sortedRight = mergeSort(right)
+
+    // console.log("sortedLeft after recursive call", sortedLeft)
+    // console.log("sortedRight after recursive call", sortedRight)
+
+    var result = [],
+        index1 = 0,
+        index2 = 0;
+
+    while(index1 < sortedLeft.length && index2 < sortedRight.length){
+        if(sortedLeft[index1] <= sortedRight[index2]){
+            result.push(sortedLeft[index1]);
+
+            index1 ++
+        }else{
+            result.push(sortedRight[index2]);
+
+            index2 ++
+        }
+    }
+
+    if (index1 === sortedLeft.length){
+     while (index2 < sortedRight.length){
+       result.push(sortedRight[index2]);
+       index2++;
+     }
+   } else {
+     while (index1 < sortedLeft.length){
+       result.push(sortedLeft[index1]);
+       index1++;
+     }
+   }
+
+    return result;
+}
 
 // 2nd try
 // function mergesort(arr){
