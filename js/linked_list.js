4358859923e533ab0a5cd6a9527ef4f2e871602f
“kasper341” <kasper_341@hotmail.com>
2017-01-18 09:28:23 -0800

    complete 'insert' method

--- js/linked_list.js
+++ js/linked_list.js
@@ -11,7 +11,7 @@ function LinkedList(){
 
 LinkedList.prototype.append = function (value) {
     
-debugger;
+// debugger;
     var node = this.head // set Node to head
 
     if (node === null){
@@ -27,11 +27,58 @@ debugger;
     }
 
     this.length ++
-    console.log(node)
+    console.log("append func ", node)
+    console.log(this.length)
+}
+
+LinkedList.prototype.insert = function (value, index){
+
+    var length = this.length,
+        currentNode = this.head,
+        previousNode = null,
+        newNode = new Node(value)
+
+    //if index if is greater than size or less than 0
+    if(index > this.length || index < 0){
+        return false
+    }
+    //if index is 0, insert at HEAD
+    if(index === 0){
+        var oldHead = this.head
+        this.head = newNode
+
+        this.head.next = oldHead
+
+        this.length += 1
+
+        return // exit
+    }
+
+    while(index < length){
+        var previous = currentNode,
+            currentNode = currentNode.next
+        console.log(currentNode)
+
+        index += 1
+    }
+
+    
+    previous.next = newNode
+    newNode.next = currentNode
+
+    this.length += 1;
+    console.log("insert func ", newNode, this.length)
+
+
 }
 
 var testCase = new LinkedList();
 testCase.append(10);
 testCase.append(30);
 testCase.append(20);
+// debugger;
+testCase.insert(11, 0)//insert at HEAD
+testCase.insert(11, -1)//insert at out of bound index
+
+testCase.insert(2, 2)//insert at given index
 // testCase.append(3);
