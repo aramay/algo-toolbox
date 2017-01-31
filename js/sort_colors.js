e99c40eb2845c935afe53f66755c86e8f296bfc1
“kasper341” <kasper_341@hotmail.com>
2017-01-31 08:50:09 -0800

    add 'sort_colors' challenge

---
+++ js/sort_colors.js
@@ -0,0 +1,13 @@
+function sortColors(args){
+
+    console.log(args)
+
+}
+
+sortColors([2, 0, 1, 2, 1, 0])
+
+/*
+Example
+Input: [2, 0, 1, 2, 1, 0]	=>	Output: [0, 0, 1, 1, 2, 2]
+Input: [1, 2, 2, 0]		=>	Output: [0, 1, 2, 2]
+*/
