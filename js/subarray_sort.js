65fc89c78781db96b684081d92e95c60004331a5
“kasper341” <kasper_341@hotmail.com>
2017-02-03 09:28:40 -0800

    add 'subarray_sort' challenge

---
+++ js/subarray_sort.js
@@ -0,0 +1,13 @@
+/**Subarray Sort
+Given an unsorted array, find the start and end index of a subarray where sorting this subarray would make the whole input array sorted.
+
+Input: Array of Integers
+Output: Two Integer Array
+Example
+Input: [3, 4, 8, 7, 10, 6, 17]   =>	Output: [2, 5]
+Input: [3, 4, 8, 7, 20, 6, 17]	=>	Output: [2, 6]
+Constraints
+Time Complexity: O(N)
+Auxiliary Space Complexity: O(1)
+ */
+
