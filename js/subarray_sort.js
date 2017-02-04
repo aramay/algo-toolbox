b47d82c6a5955d78e99300bb3764b25d9eb4018b
“kasper341” <kasper_341@hotmail.com>
2017-02-03 19:01:29 -0800

    partial implementation

--- js/subarray_sort.js
+++ js/subarray_sort.js
@@ -11,3 +11,41 @@ Time Complexity: O(N)
 Auxiliary Space Complexity: O(1)
  */
 
+
+function subArraySort(args){
+
+    console.log(args)
+    var i,
+        start = 0,
+        len = args.length - 1,
+        end = len;
+
+    //loop left-right and find element greater than next element
+    for(start = 0; start<args.length; start++){
+
+        console.log(args[i])
+
+        previous = args[i - 1]
+
+        if(args[start] > args[start + 1]){
+            console.log("found greater ", args[start])
+            break;
+        }
+
+        if(start === end){
+            console.log("array is sorted")
+        }
+    }
+
+    //loop righ-left and find element smaller than next element
+
+    for(end = len; end > 0; end--){
+
+        if(args[end] < args[end - 1]){
+            console.log("found smaller ", args[end])
+            break;
+        }
+    }
+}
+
+console.log(subArraySort([3, 4, 8, 7, 10, 6, 17]))
