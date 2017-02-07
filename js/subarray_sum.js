a3251532ef864d15b2bdfcf1a2f2a2c42985fbfd
“kasper341” <kasper_341@hotmail.com>
2017-02-07 08:19:05 -0800

    add subarrays sum problem

---
+++ js/subarray_sum.js
@@ -0,0 +1,23 @@
+/**
+ * Subarray Sum
+Given an array of positive integers and a target value, return true if there is a subarray of consecutive elements that sum up to this target value. 
+
+Input: Array of integers, target value
+Output: Boolean
+Example
+Input: [6,12,1,7,5,2,3], 14      	=>	Output: true (7+5+2)
+Input: [8,3,7,9,10,1,13], 50		=>	Output: false
+Constraints
+Time Complexity: O(N)
+Auxiliary Space Complexity: O(1)
+All elements are positive
+
+
+ */
+
+function subArraySum(args, target){
+
+    console.log(args)
+}
+
+console.log(subArraySum([6,12,1,7,5,2,3], 14))
