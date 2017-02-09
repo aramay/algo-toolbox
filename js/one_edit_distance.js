59fc3523494a6a5c30365772550c998b4ed28f4d
“kasper341” <kasper_341@hotmail.com>
2017-02-09 10:55:33 -0800

    add one_edit_distance challenge

---
+++ js/one_edit_distance.js
@@ -0,0 +1,31 @@
+/**
+ * 
+One Edit Distance
+Given two words, determine if the the first word is one edit away from the second word.
+
+
+One edit on a word can be one of the following:
+Add a character
+Remove a character
+Change the character 
+
+Input: Two Strings
+Output: Boolean
+Example
+Input: ‘table’, ‘tablet’   =>	Output: true (edits: 1)
+Input: ‘table’, ‘tale’	=>	Output: true (edits: 1)
+Input: ‘table’, ‘cables’	=>	Output: false (edits: 2)
+Constraints
+Time Complexity: O(N)
+Auxiliary Space Complexity: O(1)
+
+If the two inputs strings are the same, return false
+Assume only lowercase letters, no spaces
+ */
+
+function oneEditDistance(args){
+
+    console.log(args)
+}
+
+console.log(oneEditDistance("table"))
