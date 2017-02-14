780f77ae1aa7c36ecc9a96dba96193d9990cb9bb
“kasper341” <kasper_341@hotmail.com>
2017-02-13 20:43:46 -0800

    implement one_edit_distance

--- js/one_edit_distance.js
+++ js/one_edit_distance.js
@@ -25,10 +25,59 @@ Assume only lowercase letters, no spaces
 
 function oneEditDistance(args, target){
 
-    console.log(args)
+    // console.log(args)
 
+    var len1 = args.length,
+        len2 = target.length,
+        idx1 = 0,
+        idx2 = 0,
+        edits = 0;
+
+    while(idx1 < len1){
+
+console.log(idx1)
+
+        if(args[idx1] === target[idx2]){
+
+            idx1 ++
+            idx2 ++
+
+            console.log("first if")
+        }
+        else{
+
+            if(edits > 1){
+                return false
+            }
+
+            edits += 1
+
+            console.log(edits)
+
+            if(len1 === len2){
+                idx1 ++
+                idx2 ++
+            }
+            else if(args.length > target.length){
+                idx1 ++
+            }
+            else{
+                idx2 ++
+            }
+        }
+    }
     
+
+    edits === 1 ? true : false
+    // if(edits > 1){
+    //     return false
+    // }
+    // else{
+    //     return true
+    // }
 
 }
 
-console.log(oneEditDistance("table"))
+// console.log(oneEditDistance("table", "tablet"))
+// console.log(oneEditDistance("table", "tale"))
+console.log(oneEditDistance("table", "cables"))
