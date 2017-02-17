6b12143e876995867ca616f8e9041f7d15f7c0c0
“kasper341” <kasper_341@hotmail.com>
2017-02-16 16:11:01 -0800

    add new challenges

---
+++ js/all_anagrams.js
@@ -0,0 +1,30 @@
+var allAnagrams = function(input){
+  var results = [];
+  // turn input into array
+  work = input.split('');
+
+  // create recursive function
+  var buildCombinations = function(buildUp, pullFrom){
+    if (pullFrom.length === 0){
+      results.push(buildUp);
+      return;
+    }
+
+    for (var i = 0; i < pullFrom.length; i++){
+      var temp = pullFrom.splice(i, 1);
+      buildCombinations(buildUp + temp, pullFrom);
+      pullFrom.splice(i, 0, temp);
+    }
+  }
+
+  // call recursive function
+  buildCombinations('', work);
+
+  // return results
+  console.log(results);
+  return results;
+}
+
+allAnagrams('abcc');
+
+// http://stackoverflow.com/questions/909449/anagrams-finder-in-javascript
