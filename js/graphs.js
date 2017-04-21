49c44c6c7edfe3f1f74eab2a1c7b65215d31479b
“kasper341” <kasper_341@hotmail.com>
2017-04-20 18:51:23 -0700

    add graphs DS

---
+++ js/graphs.js
@@ -0,0 +1,134 @@
+ /*
+  *                                 Homework VIII                                  
+  *                                                                                
+  *  Problem: Graph                                                                
+  *                                                                                
+  *  Prompt: Create a Vertex class/constructor and Graph class/constructor.        
+  *          Name it Graph.                                                        
+  *                                                                                
+  *  The Vertex will have the following properties:                                 
+  *                                                                                
+  *               value: integer value (initially null)                            
+  *               edges: hash that contains edges to other vertices                
+  *                                                                                
+  *  The Graph will have the following properties:                                 
+  *                                                                                
+  *            vertices: A hash/dictionary/object to store vertices                
+  *       totalVertices: The total number of vertices in your Graph                
+  *          totalEdges: The total number of edges in your Graph                   
+  *                                                                                
+  *  The Graph will also have the following methods:                               
+  *                                                                                
+  *           addVertex: Method that accepts an id (int/str), and creates an       
+  *                      object with a "value" of id, and a property called        
+  *                      "edges" which will store the edges of the vertex. If a    
+  *                      vertex with the id already exists, then do not create a   
+  *                      new vertex.                                               
+  *                                                                                
+  *           getVertex: Method that takes an id, and outputs the vertex with the  
+  *                      matching id, if it exists.                                
+  *                                                                                
+  *             addEdge: Method that accepts two different id's as its input, and  
+  *                      creates an edge between both vertices.                    
+  *                      (This edge may look like [id1,id2])                       
+  *                                                                                
+  *          removeEdge: Method that accepts two different id's as its input, and  
+  *                      removes the edge between the two vertices if it exists.   
+  *                                                                                
+  *        removeVertex: Method that takes an id as its input, and removes the     
+  *                      vertex with the matching id.                              
+  *                                                                                
+  *       findNeighbors: Method that accepts an id as its input, and returns       
+  *                      all of the edges of that vertex.                          
+  *                                                                                
+  *       forEachVertex: Method that accepts an operation, and performs that       
+  *                      operation on each vertex in the Graph.                    
+  *                                                                                
+  *  Input:  N/A                                                                   
+  *  Output: A Graph instance                                                      
+  *                                                                                
+  *  What are the time and auxilliary space complexities of the various methods?   
+  *                                                                                
+  */
+
+'use strict';
+
+ /*
+  *  Extra Credit: forEachEdge
+  *
+  *  Prompt: Method that accepts an operation, and performs that operation on each
+  *          edge in the Graph
+  */
+ 
+
+
+
+var Vertex = function(id){
+
+};
+
+
+var Graph = function(){
+
+};
+
+
+// Time Complexity: 
+// Auxiliary Space Complexity: 
+Graph.prototype.addVertex = function(id) {
+
+};
+
+
+// Time Complexity: 
+// Auxiliary Space Complexity: 
+Graph.prototype.getVertex = function(id) {
+
+};
+
+
+// Time Complexity: 
+// Auxiliary Space Complexity: 
+Graph.prototype.addEdge = function(id1, id2) {
+
+};
+
+
+// Time Complexity: 
+// Auxiliary Space Complexity: 
+Graph.prototype.removeEdge = function(id1, id2) {
+
+};
+
+
+// Time Complexity: 
+// Auxiliary Space Complexity: 
+Graph.prototype.removeVertex = function(id) {
+
+};
+
+
+// Time Complexity: 
+// Auxiliary Space Complexity: 
+Graph.prototype.findNeighbors = function(id) {
+
+};
+
+
+// Time Complexity: 
+// Auxiliary Space Complexity: 
+Graph.prototype.forEachVertex = function(operation) {
+
+};
+
+
+// Time Complexity: 
+// Auxiliary Space Complexity: 
+Graph.prototype.forEachEdge = function(operation) {
+
+};
+
+
+
+
+
