1ee4b450fffe438c1cf2ed5915e6518d9149c9a7
aramay <kasper_341@hotmail.com>
2019-05-26 11:48:06 -0700

    fix linting errors

--- python/2048-game.py
+++ python/2048-game.py
@@ -6,6 +6,7 @@ def merge(line):
    """
    Function that merges a single row or column in 2048.
    """
+   print("input list ", line)
    out_list = [0] * len(line)
    current_index = 0
    result = []
@@ -13,41 +14,45 @@ def merge(line):
    print("start out_list ", out_list)
    
    for i in range(len(line)):
-      # print(i)
-      
-      if line[i] == 0:
-         #do not change
-         pass
-      else:
-         #current_index = 0
-         #print("current_index ", current_index)
+      """
+      Iterate over the input and create an output list that has all of the non-zero tiles slid over to the beginning of the list with the appropriate number of zeroes at the end of the list.
+      """
+      if line[i] != 0:
          out_list[current_index] = line[i]
          current_index += 1
 
    print("out_list done ", out_list)
 
    while n < len(out_list):
+      """
+      create sub-list of 2 pairs from previous step -> out_list
+      """
       
-
       sub_list = out_list[n:n+2]
-      print(sub_list)
+      print("sub list ", sub_list)
       print(len(sub_list))
 
+      # 1 - check if sub-list has only 1 element
+      # on last iteration, prvenent indexOutOfBound error
       if len(sub_list) < 2:
-         print("less than 2 ")
+         print("less than 2 ", result)
          result.append(sub_list[0])
 
          n += 1
+      # 2 - check if two pairs are same value
+      # if yes -> then add 
+      # append to result list
       elif sub_list[0] == sub_list[1]:
          result.append(sub_list[0] * 2)
 
          print("result list 2nd iter ", result)
 
          n += 2
+      
+      # 3 - if two pairs are not same value
+      # then push only non-zero value to result
       else:
-         # (result.append(n) for n in sub_list)
          result.append(sub_list[0])
-         # result.append(sub_list[1])
          print("result list 3rd iter ", result)
          
          n += 1
@@ -59,31 +64,6 @@ def merge(line):
          result.append(0)
       print ("final iter ", result)
       
-   # for x in range(len(out_list)):
-   #    print ("x ", x)
-   #    if out_list[x] == out_list[x+1]:
-   #       # out_list[x] = out_list[x] * 2
-   #       result.append(out_list[x] + out_list[x+1])
-   #       # out_list[x + 1] = 0
-   #    else:
-   #       result.append(out_list[x])
-
-   #    print ("result ", result)
-      # return out_list
-      #print(len(out_list))
-   #   elif out_list[x] == out_list[current_index1]:
-         
-   #       sum = out_list[x] + out_list[current_index1]
-   #       line[x] = sum
-   #       current_index1 += 1
-               
-   #   else:
-   #       line[x] = out_list[current_index1]
-   #       current_index1 += 1     
-      
-         
-   print ("line ", line)
-   # print ("result ", out_list)
    return result
 
 
@@ -91,9 +71,9 @@ result = merge([2,0,2,4])
 
 print ([4, 4, 0, 0] == result)
 
-result1 = merge([4, 2, 2])
+# result1 = merge([4, 2, 2])
 
-print ( [4, 4, 0] == result1)
+# print ( [4, 4, 0] == result1)
 
 """
 [2,0,2,4] should return 
