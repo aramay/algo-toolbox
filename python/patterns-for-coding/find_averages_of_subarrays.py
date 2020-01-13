73138de1e054116d4a454f8390298e8f78512b4a
aramay <kasper_341@hotmail.com>
2020-01-13 07:49:04 -0800

    finish first solution

--- python/patterns-for-coding/find_averages_of_subarrays.py
+++ python/patterns-for-coding/find_averages_of_subarrays.py
@@ -5,13 +5,16 @@ import unittest
 
 
 def find_avg_subarry(arr, k):
+
+   """
+   Time complexity: Since for every element of the input array, 
+   we are calculating the sum of its next ‘K’ elements, 
+   the time complexity of the above algorithm will be 
+   O(N*K)
+   where ‘N’ is the number of elements in the input array.
+   """
    
    result = []
-
-   # outer_loop = range(len(arr) - (k+1))
-   # l = len(arr)
-   # end = k+1
-   # outer_loop = l - end
 
    for i in range(len(arr) - k + 1):
 
@@ -19,19 +22,27 @@ def find_avg_subarry(arr, k):
 
       for j in range(i, i+k):
 
-         # print (arr[j])
          sum += arr[j]
-      result.append(sum / 5)
+      result.append(sum / k)
       print("first 5 elemt")
-      print("result ", result)
+   print("result ", result)
+
+   return result
 
 
 class Test(unittest.TestCase):
    
 
-   def test_avd(self):
+   def test_avg_1(self):
       result = find_avg_subarry([1, 3, 2, 6, -1, 4, 1, 8, 2], 5)
 
-      # assert result == 
+      print(result)
+      assert result == [2.2, 2.8, 2.4, 3.6, 2.8]
+   
+   def test_avg_2(self):
+      result = find_avg_subarry([1, 3, 2, 6, -1], 5)
+
+      print(result)
+      assert result == [2.2]
 
 unittest.main(verbosity=2)
