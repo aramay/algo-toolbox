04d891a3886dcdcbb29af312e193d200e0e1ba1e
aramay <kasper_341@hotmail.com>
2020-01-15 08:07:32 -0800

    add smallest_subarray_with_given_sum.py

---
+++ python/patterns-for-coding/smallest_subarray_with_given_sum.py
@@ -0,0 +1,46 @@
+"""
+Problem Statement #
+Given an array of positive numbers and a positive number ‘S’, 
+find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
+
+Example # 1
+Input: [2, 1, 5, 2, 3, 2], S=7 
+Output: 2
+Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 2].
+
+Example # 2
+Input: [2, 1, 5, 2, 8], S=7 
+Output: 1
+Explanation: The smallest subarray with a sum greater than or equal to '7' is [8].
+
+Example # 3
+Input: [3, 4, 1, 1, 6], S=8 
+Output: 3
+Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] or [1, 1, 6].
+"""
+
+import unittest
+
+def smallest_subarray_with_given_sum(arr, s):
+   window_sum = 0
+   windows_start = 0
+   min_length = 0
+
+   for windows_end in range(len(arr)):
+      
+      window_sum += arr[windows_end]
+
+      while window_sum >= s:
+         pass
+   
+   return window_sum
+
+class Test(unittest.TestCase):
+
+   def test_smallest_subarray_with_given_sum(self):
+      result = smallest_subarray_with_given_sum([2, 1, 5, 2, 3, 2], s=7)
+
+      assert result == 2
+
+
+unittest.main(verbosity=2)
