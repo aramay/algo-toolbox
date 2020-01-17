68d628e82611e665380c5efc68b44a8a0f2589e5
aramay <kasper_341@hotmail.com>
2020-01-17 07:29:01 -0800

    add python/patterns-for-coding/longest_substring_with_k_distinct.py

---
+++ python/patterns-for-coding/longest_substring_with_k_distinct.py
@@ -0,0 +1,31 @@
+"""
+Problem Statement #
+Given a string, find the length of the longest substring in it with no more than K distinct characters.
+
+Example#1
+Input: String="araaci", K=2
+Output: 4
+Explanation: The longest substring with no more than '2' distinct characters is "araa".
+
+Example#2
+Input: String="araaci", K=1
+Output: 2
+Explanation: The longest substring with no more than '1' distinct characters is "aa".
+
+Example#3
+Input: String="cbbebi", K=3
+Output: 5
+Explanation: The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
+"""
+import unittest
+
+def longest_substring_with_k_distinct(str, k):
+   pass
+
+
+class Test(unittest.TestCase):
+   
+   def test_longest_substring_with_k_distinct(self):
+      pass
+
+unittest.main(verbosity=2)
