7392af5437cd615c0901e608c429dd406049b5bc
aramay <kasper_341@hotmail.com>
2020-01-24 08:06:32 -0800

    add file non_repeat_substring.py

---
+++ python/patterns-for-coding/non_repeat_substring.py
@@ -0,0 +1,19 @@
+"""
+Problem Statement #
+Given a string, find the length of the longest substring which has no repeating characters.
+
+Example 1:
+Input: String="aabccbb"
+Output: 3
+Explanation: The longest substring without any repeating characters is "abc".
+
+Example 2:
+Input: String="abbbb"
+Output: 2
+Explanation: The longest substring without any repeating characters is "ab".
+
+Example 3:
+Input: String="abccde"
+Output: 3
+Explanation: Longest substrings without any repeating characters are "abc" & "cde".
+"""
