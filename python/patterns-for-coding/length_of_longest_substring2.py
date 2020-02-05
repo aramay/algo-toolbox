ee82184dc889885469df481f3b8a2438ba2968ff
aramay <kasper_341@hotmail.com>
2020-02-05 07:39:39 -0800

    finsish python/patterns-for-coding/length_of_longest_substring2.py

--- python/patterns-for-coding/length_of_longest_substring2.py
+++ python/patterns-for-coding/length_of_longest_substring2.py
@@ -1,6 +1,7 @@
 """
 Problem Statement #
-Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s, find the length of the longest contiguous subarray having all 1s.
+Given an array containing 0s and 1s, if you are allowed to replace no more than 
+‘k’ 0s with 1s, find the length of the longest contiguous subarray having all 1s.
 
 Example 1:
 Input: Array=[0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2
@@ -16,10 +17,29 @@ Explanation: Replace the '0' at index 6, 9, and 10 to have the longest contiguou
 import unittest
 
 def len_of_longest_substring(str, k):
-   pass
+   window_start, max_one_count, max_len = 0,0,0
+   frequency_map = {}
+
+   for window_end in range(len(str)):
+      if str[window_end] == 1:
+         max_one_count += 1
+
+      if (window_end - window_start + 1 - max_one_count) > k:
+         if str[window_start] == 1:
+            max_one_count -= 1
+         window_start += 1
+      
+      max_len = max(max_len, window_end - window_start + 1)
+   
+   return max_len
+
 
 class Test(unittest.TestCase):
-   pass
+
+   def test_len_of_longest_substring(self):
+      result = len_of_longest_substring(str=[0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2)
+
+      assert result == 6
 
 
 unittest.main(verbosity=2)
