0539e813c65de20a7942ecb8132cf9f77b8a2617
aramay <kasper_341@hotmail.com>
2020-02-11 06:09:05 -0800

    add method find_permutation

---
+++ python/patterns-for-coding/find_permutation.py
@@ -0,0 +1,39 @@
+"""
+Permutation in a String (hard) #
+Given a string and a pattern, find out if the string contains any permutation of the pattern.
+
+Permutation is defined as the re-arranging of the characters of the string. For example, “abc” has the following six permutations:
+
+abc
+acb
+bac
+bca
+cab
+cba
+If a string has ‘n’ distinct characters it will have 
+n
+!
+n! permutations.
+
+Example 1:
+Input: String="oidbcaf", Pattern="abc"
+Output: true
+Explanation: The string contains "bca" which is a permutation of the given pattern.
+
+Example 2:
+Input: String="odicf", Pattern="dc"
+Output: false
+Explanation: No permutation of the pattern is present in the given string as a substring.
+
+"""
+
+import unittest
+
+def find_permutation(str, pattern):
+   pass
+
+
+class Test(unittest.TestCase):
+   pass
+
+unittest.main(verbosity=2)
