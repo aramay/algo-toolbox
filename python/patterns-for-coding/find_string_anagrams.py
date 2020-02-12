450c8bc32a906e498495bcb491d5dfab5293f045
aramay <kasper_341@hotmail.com>
2020-02-12 06:31:28 -0800

    add file find_string_anagrams.py

---
+++ python/patterns-for-coding/find_string_anagrams.py
@@ -0,0 +1,27 @@
+"""
+Given a string and a pattern, find all anagrams of the pattern in the given string.
+
+Anagram is actually a Permutation of a string. For example, “abc” has the following six anagrams:
+
+Write a function to return a list of starting indices of the anagrams of the pattern in the given string.
+
+Example 1:
+Input: String="ppqp", Pattern="pq"
+Output: [1, 2]
+Explanation: The two anagrams of the pattern in the given string are "pq" and "qp".
+
+Example 1:
+Input: String="abbcabc", Pattern="abc"
+Output: [2, 3, 4]
+Explanation: The three anagrams of the pattern in the given string are "bca", "cab", and "abc".
+"""
+import unittest
+
+def find_string_anagram():
+   pass
+
+
+class Test(unittest.TestCase):
+   pass
+
+unittest.main(verbosity=2)
