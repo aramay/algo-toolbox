b38e7cc4996d05baf2e8749a4cbfd227db4bd7dc
aramay <kasper_341@hotmail.com>
2020-02-20 07:03:41 -0800

    add file -> find peek

---
+++ python/DS-Algos-in-Python/mit-606/peek_finding.py
@@ -0,0 +1,15 @@
+"""
+Peak Finder One-dimensional Version
+Position2 is a peak if and only if b≥a and b≥c. Position 9 is a peak if i≥h.
+"""
+
+import unittest
+
+def find_peek():
+   pass
+
+class Test(unittest.TestCase):
+   pass
+
+
+unittest.main(verbosity=2)
