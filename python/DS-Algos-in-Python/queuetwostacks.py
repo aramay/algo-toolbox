e9cef8430c5f47f21b6f39115aeb99c981b09b01
aramay <kasper_341@hotmail.com>
2020-04-07 07:25:40 -0700

    finsih two stacks -> queue solution

--- python/DS-Algos-in-Python/queuetwostacks.py
+++ python/DS-Algos-in-Python/queuetwostacks.py
@@ -5,33 +5,32 @@ class QueueTwoStacks(object):
 
     # Implement the enqueue and dequeue methods
 
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
 
     def enqueue(self, item):
-        pass
+        self.in_stack.append(item)
 
     def dequeue(self):
-        pass
+        if len(self.out_stack) == 0:
+            
+            while len(self.in_stack) > 0:
+                new_item = self.in_stack.pop()
+                self.out_stack.append(new_item)
 
+            if len(self.out_stack) == 0:
+                raise IndexError("Can't deque from empty stack")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return self.out_stack.pop()
 
 
 
 # Tests
 
 class Test(unittest.TestCase):
+
 
     def test_basic_queue_operations(self):
         queue = QueueTwoStacks()
