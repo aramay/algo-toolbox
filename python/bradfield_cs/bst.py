9b262c98dc09e2991bd198e2e0d357e6827ce333
aramay <kasper_341@hotmail.com>
2020-04-19 13:51:52 -0700

    add bst file

---
+++ python/bradfield_cs/bst.py
@@ -0,0 +1,15 @@
+"""
+https://bradfieldcs.com/algos/searching/the-binary-search/
+
+"""
+
+import unittest 
+
+
+
+class TestBST(unittest.TestCase):
+
+   def test_bst(self):
+      pass
+
+unittest.main(verbosity=2)
