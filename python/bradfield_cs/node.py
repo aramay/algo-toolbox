8d293a216d3cdd4d2d5266966008a1778c7d4d2f
aramay <kasper_341@hotmail.com>
2020-04-26 14:04:15 -0700

    add method to add leftChild

--- python/bradfield_cs/node.py
+++ python/bradfield_cs/node.py
@@ -5,11 +5,42 @@ https://bradfieldcs.com/algos/trees/representing-a-tree/
 
 import unittest
 
-def Node(self, leftChild, rightChild):
-   pass
+class Node():
+
+   def __init__(self, val, leftChild=None, rightChild=None):
+      self.val = val
+      self.rightChild = rightChild
+      self.leftChild = leftChild
+
+   def insert_left(self, child):
+      if self.leftChild is None:
+         self.leftChild = child
+      else:
+         child.leftChild = self.leftChild
+         self.leftChild = child
 
 class Test(unittest.TestCase):
-   pass
+   def test_create_node(self):
+      x = Node(3)
+
+      print("\n", x.__dict__)
+
+   def test_insert_left_child(self):
+      root = Node(4)
+      
+      print("\n", root.__dict__)
+      print("\n", root.val)
+      print("\n", root.leftChild)
+
+      root.insert_left(Node("left node"))
+
+      print("\n", root.__dict__)
+
+
+
+
+
+
 
 
 unittest.main(verbosity=2)
