1b135124b129d5b188d0d28435b5d65361d54e14
aramay <kasper_341@hotmail.com>
2020-06-30 08:27:10 -0700

    remove fruits_into_baskets implementation - do it again ga python/patterns-for-coding/fruits_into_baskets.py

--- python/patterns-for-coding/fruits_into_baskets.py
+++ python/patterns-for-coding/fruits_into_baskets.py
@@ -20,30 +20,7 @@ This can be done if we start with the second letter: ['B', 'C', 'B', 'B', 'C']
 import unittest
 
 def fruits_into_baskets(fruits):
-   window_start = 0
-   fruit_frequency = {}
-   max_length = 0
-
-   for window_end in range(len(fruits)):
-      right_fruit = fruits[window_end]
-
-      if right_fruit not in fruit_frequency:
-         fruit_frequency[right_fruit] = 0
-      fruit_frequency[right_fruit] += 1
-
-      while len(fruit_frequency) > 2:
-         left_fruit = fruits[window_start]
-
-         fruit_frequency[left_fruit] -= 1
-
-         if fruit_frequency[left_fruit] == 0:
-            del fruit_frequency[left_fruit]
-
-         window_start += 1
-
-      max_length = max(max_length, window_end-window_start + 1)
-
-   return max_length
+   
 
 
 
