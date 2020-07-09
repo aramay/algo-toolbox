e99bc8a81d96f3dc86bfd79ce2e6eaee2f169a5a
aramay <kasper_341@hotmail.com>
2020-07-09 06:56:47 -0700

    2nd attempt

--- python/patterns-for-coding/non_repeat_substring.py
+++ python/patterns-for-coding/non_repeat_substring.py
@@ -22,36 +22,26 @@ Explanation: Longest substrings without any repeating characters are "abc" & "cd
 import unittest
 
 def non_repeat_substring(str):
-   window_start = 0
-   max_length = 0
-
-   non_repeat_chr = {}
-
-   for window_end in range(len(str)):
-      right_chr = str[window_end]
-      # if the map already contains the 'right_char', shrink the window from the beginning so that
-      # we have only one occurrence of 'right_char'
-
-      if right_chr in non_repeat_chr:
-         
-         window_start = max(window_start, non_repeat_chr[right_chr] + 1)
-      
-      non_repeat_chr[str[window_end]] = window_end
-      max_length = max(max_length, window_end - window_start + 1)
-   return max_length
+   
 
 
 class Test(unittest.TestCase):
-   def test_non_repeat_substring(self):
-      result = non_repeat_substring("aabccbb")
+   # def test_non_repeat_substring(self):
+   #    result = non_repeat_substring("aabccbb")
+   #    print(result)
+
+   #    assert result == 3
+   
+   # def test_non_repeat_substring1(self):
+   #    result = non_repeat_substring("aabccbb")
+   #    print(result)
+
+   #    assert result == 2
+   
+   def test_non_repeat_substring2(self):
+      result = non_repeat_substring("ABCBA")
       print(result)
 
       assert result == 3
-   
-   def test_non_repeat_substring1(self):
-      result = non_repeat_substring("aaaaaaa")
-      print(result)
-
-      assert result == 1
 
 unittest.main(verbosity=2)
