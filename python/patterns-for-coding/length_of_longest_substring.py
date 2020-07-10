8c70edb3ad3f077ada018ab04575fcdbc10be57d
aramay <kasper_341@hotmail.com>
2020-07-10 07:11:46 -0700

    redo this problem

--- python/patterns-for-coding/length_of_longest_substring.py
+++ python/patterns-for-coding/length_of_longest_substring.py
@@ -21,29 +21,7 @@ import unittest
 
 def len_of_longest_sustring(str, k):
    
-   frequency_map = {}
-   window_start, max_repeat_letter_count, max_length = 0, 0, 0
-
-   for window_end in range(len(str)):
-      right_char = str[window_end]
-
-      if right_char not in frequency_map:
-         frequency_map[right_char] = 0
-      frequency_map[right_char] += 1
-      
-      max_repeat_letter_count = max(max_repeat_letter_count, frequency_map[right_char])
-
-      count = window_end - window_start + 1 - max_repeat_letter_count
-
-      if (count) > k:
-         left_char = str[window_start]
-
-         frequency_map[left_char] -= 1
-         window_start += 1
-
-      max_length = max(max_length, window_end - window_start + 1)
-
-   return max_length
+  
 
 
 class Test(unittest.TestCase):
