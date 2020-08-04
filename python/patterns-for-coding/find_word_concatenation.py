8b96106a7a876284b9967df5a2ccba4a6b8f569f
aramay <kasper_341@hotmail.com>
2020-08-04 07:41:31 -0700

    add 1st solution

--- python/patterns-for-coding/find_word_concatenation.py
+++ python/patterns-for-coding/find_word_concatenation.py
@@ -17,12 +17,56 @@ Explanation: The only substring containing both the words is "catfox".
 Example 3:
 """
 
-def find_word_concat(input:str, word:str) -> str:
+def find_word_concat(input:str, words:str) -> str:
+   result_indices = []
 
-   pass
+   word_frequency = {}
+
+   for word in words:
+      if word not in word_frequency:
+         word_frequency[word] = 0
+      word_frequency[word] = 0
+
+   word_len = len(word[0])
+   words_count = len(words)
+
+   for i in range((len(input) - words_count * word_len) + 1):
+      
+      words_seen = {}
+      
+      for j in range(0, words_count):
+
+         next_word_index = i + j * word_len
+
+         word = input[next_word_index: next_word_index + word_len]
+
+         if word not in word_frequency:
+            break
+
+         # add word to word seen map
+         if word not in words_seen:
+            words_seen[word] = 0
+         words_seen[word] += 1
+
+         # no need to process further
+         if words_seen[word] > word_frequency.get(word, 0):
+            break
+
+         # store index if we've seen all words
+         if j + 1 == words_count:
+            result_indices.append(i)
+
+
+
+
+   return result_indices
 
 
 class TestWordConcat(unittest.TestCase):
-   pass
+   
+   def test_word_count(self):
+      result = find_word_concat(input="catfoxcat", words=["cat", "fox"])
+
+      assert result == [0, 3]
 
 unittest.main(verbosity=2)
