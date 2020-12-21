52d5e1ec8655010de838060e62cafc93c2abd759
aramay <kasper_341@hotmail.com>
2020-12-20 19:33:35 -0800

    update filter mehtod to use arrow function

--- js/numbersInRange.js
+++ js/numbersInRange.js
@@ -1,9 +1,9 @@
 // return the number in a given range
-function NumbersInRange(input, low, high) {
-   this.input = input
-   this.low = low
-   this.high = high
-   this.result = []
+function NumbersInRange (input, low, high) {
+  this.input = input
+  this.low = low
+  this.high = high
+  this.result = []
 }
 
 NumbersInRange.prototype.getNumbersInRange = function () {
@@ -12,15 +12,14 @@ NumbersInRange.prototype.getNumbersInRange = function () {
 //   console.log("low ", this.low)
   this.result = temp.filter(cbf)
 
-  function cbf(n, index){
-   NumbersInRange.call(this, low, high)
-   console.log(Array.from(arguments))
+  function cbf (n, index) {
+    NumbersInRange.call(this, low, high)
+    console.log(Array.from(arguments))
 
  //   console.log(this.NumbersInRange)
-   if(n >= low && n <= high) {
+    if (n >= low && n <= high) {
       return n
-   }
-
+    }
   }
    /*this.result = temp.filter(function (n, index) {
 
@@ -29,9 +28,8 @@ NumbersInRange.prototype.getNumbersInRange = function () {
      if(n > low && n < high){
         return n
      }
-  })*/
+  }) */
 
-   
   // this implementation fails test - cuz it needs a short circut operator
   /*for (let i = 0; i < temp.length; i++) {
     if (temp[i] > this.low){
@@ -40,23 +38,18 @@ NumbersInRange.prototype.getNumbersInRange = function () {
     else if (temp[i] < this.high) {
       this.result.push(temp[i])
     }
-  }*/
-
+  } */
 }
 
-function assertEquals(actual, expected, testName) {
+function assertEquals (actual, expected, testName) {
 //   console.log(Array.from(arguments))
-  
   let isEqualLen = actual.length === expected.length
-  
-  if(isEqualLen){
+  if (isEqualLen) {
     console.log('passed')
-  }
-  else {
+  }  else {
     console.log(`FAILED! [${testName}] Expected ${expected} but got ${actual}`)
   }
 }
-
 
 let input = [3, 34, 4, 12, 5, 2]
 let low = 1
