33968b51ec79d8d154b23a73ddb8d605aa1d9866
aramay <kasper_341@hotmail.com>
2022-01-10 15:52:52 -0800

    fix linting errors

--- js/patterns-for-conding/max_sub_array_of_size_k.js
+++ js/patterns-for-conding/max_sub_array_of_size_k.js
@@ -3,48 +3,52 @@
 https://www.educative.io/courses/grokking-the-coding-interview/JPKr0kqLGNP
  */
 
-const max_sub_array_of_size_k = (k, input) => {
+
+
+const maxSubArrayOfSizeK = (k, input) => {
   console.log('function called')
   let max_sum = 0
 
-  const calculate_sum = (temp, i) => {
+  // helper method to calculate subarray sum
+  const calculateSum = (temp) => {
     let window_sum = 0
 
     for (let j = 0; j < temp.length; j++) {
       window_sum += temp[j]
     }
-    // update max_sum
-    // if (window_sum > max_sum) {  
-    //   max_sum = window_sum
-    // }
-
+    // calculate max of (max_sum and widow_sum)
     max_sum = Math.max(max_sum, window_sum)
   }
 
-  for(let i = 0; i < input.length; i++) {
-    
+  for (let i = 0; i < input.length; i++) {
     let temp = input.slice(i, k + i)
-
-    calculate_sum(temp, i)
+    // call helper method to calculate sum
+    calculateSum(temp, i)
   }
   return max_sum
 }
 
+/** 
+ * Input: [2, 1, 5, 1, 3, 2], k=3
+Output: 9
+Explanation: Subarray with maximum sum is [5, 1, 3].
+ */
+
 let input1 = [2, 1, 5, 1, 3, 2]
 let k = 3
-let result = max_sub_array_of_size_k(k, input1) // [5, 1, 3] => 9
+let result = maxSubArrayOfSizeK(k, input1) // [5, 1, 3] => 9
 
 console.log('result ', result)
 
 
 /**
- * Input: 
+ * Input: [2, 3, 4, 1, 5]
 Output: 7
 Explanation: Subarray with maximum sum is [3, 4]. => 7
  */
 
 const input2 = [2, 3, 4, 1, 5]
 let k1 = 2
-result = max_sub_array_of_size_k(k1, input2) 
+result = maxSubArrayOfSizeK(k1, input2)
 
 console.log('result2 ', result)
