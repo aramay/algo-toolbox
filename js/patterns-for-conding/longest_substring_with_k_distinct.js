cd48d2005e3f069ae637cc7cbf7b60364519a09e
aramay <kasper_341@hotmail.com>
2022-01-15 19:33:19 -0800

    in progress - longestSubstringWithKDistinct chars

--- js/patterns-for-conding/longest_substring_with_k_distinct.js
+++ js/patterns-for-conding/longest_substring_with_k_distinct.js
@@ -22,8 +22,39 @@ Output: 6
 Explanation: The longest substring with no more than '10' distinct characters is "cbbebi".
  */
 
-const longestSubstringWithKDistinct = () => {
+const longestSubstringWithKDistinct = (arr1, k) => {
   console.log('inside func')
+  let windowEnd = 0
+  let windowStart = 0
+  let charCount = {}
+
+  let maxSubStr = 0
+
+  for (windowEnd ; windowEnd < arr1.length; windowEnd++) {
+    let temp = arr1[windowEnd]
+    // check if key exists
+    if(charCount[temp] === undefined) {
+      charCount[arr1[windowEnd]] = 1
+    } else {
+      charCount[temp] += 1
+    }
+
+    maxSubStr += 1
+
+    while (Object.keys(charCount).length >= k) {
+      maxSubStr -= 1
+      windowStart += 1
+    }
+
+  }
+  console.log(charCount)
+  return maxSubStr
 }
 
-longestSubstringWithKDistinct("cbbebi")
+let arr1 ="araaci"
+let K=2
+
+const res1 = longestSubstringWithKDistinct(arr1, K) //Output: 4
+
+console.log('res1 ', res1)
+
