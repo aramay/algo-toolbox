526be17fa82ca4944e20f0db2f85c3e752c9fa3e
aramay <kasper_341@hotmail.com>
2022-01-17 13:57:55 -0800

    add linear kadane's algo solution

--- js/patterns-for-conding/max_subarray.js
+++ js/patterns-for-conding/max_subarray.js
@@ -29,3 +29,25 @@ Constraints:
 Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
  */
 
+const maxSubArr = (arr1) => {
+  console.log('func called')
+  let maxSum = -Infinity
+  let sum = 0
+
+  for(let i = 0; i < arr1.length; i++) {
+    sum += arr1[i]
+    // if currentSum is > max, then reassign maxSum
+    if(sum > maxSum) {
+      maxSum = sum
+    }
+    // if currentSum < 0, then set currentSum to 0
+    if (sum < 0) sum = 0
+    
+  }
+  return maxSum
+}
+
+let nums = [-2,1,-3,4,-1,2,1,-5,4]
+let res1 = maxSubArr(nums)
+
+console.log('max sub array sum ', res1)
