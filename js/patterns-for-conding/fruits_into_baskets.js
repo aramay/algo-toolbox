07f79b082966db413a59651a3bd928d56563728a
aramay <kasper_341@hotmail.com>
2022-01-26 09:34:03 -0800

    add solution fruitsIntoBasket

--- js/patterns-for-conding/fruits_into_baskets.js
+++ js/patterns-for-conding/fruits_into_baskets.js
@@ -21,13 +21,43 @@ This can be done if we start with the second letter: ['B', 'C', 'B', 'B', 'C']
 
  */
 
-const fruitsIntoBasket = () => {
-  console.log('func called ')
+const fruitsIntoBasket = (fruits) => {
+  console.log('in the func')
+  let winStart = 0
+  let fruitFrequency = {}
+  let max = 0
+
+  for (let winEnd = 0; winEnd < fruits.length; winEnd++) {
+    let temp = fruits[winEnd]
+    
+    if (fruitFrequency[temp] === undefined) {
+      fruitFrequency[temp] = 1
+    } else {
+      fruitFrequency[temp] += 1
+    }
+    console.log('fruitFrequency ', fruitFrequency)
+    
+    while (Object.keys(fruitFrequency).length > 2) {
+      let temp = fruits[winStart]
+      console.log('temp while loop', temp)
+      
+      fruitFrequency[temp] -= 1
+  
+      if (fruitFrequency[temp] === 0) {
+        delete fruitFrequency[temp]
+      }
+      winStart += 1
+      
+    }
+    max = Math.max(max, winEnd - winStart + 1)
+  }
+  // console.log(fruits.slice(winStart, winEnd))
+  return max
 }
 
 let Fruit=['A', 'B', 'C', 'A', 'C']
 
 const res1 = fruitsIntoBasket(Fruit) //Output: 3
 
-
+console.log('res1 ', res1)
 
