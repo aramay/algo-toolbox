4981b4337475c0a2f6b8bc29ad7ce61965d5d62a
aramay <kasper_341@hotmail.com>
2022-02-02 16:14:18 -0800

    complete - findPermutations problem

--- js/patterns-for-conding/find_permutation.js
+++ js/patterns-for-conding/find_permutation.js
@@ -39,14 +39,61 @@ Output: true
 Explanation: The string contains "acb" which is a permutation of the given pattern.
  */
 
-const findPermutation = (str) => {
+const findPermutation = (str, pattern) => {
   console.log('func called ', str)
+  const patternFrequencyMap = {}
+  let winStart = 0
+  let k = pattern.length
+  let matched = 0
+
+  // Create a HashMap to calculate the frequencies of all characters in the pattern.
+  for(let i = 0; i < pattern.length; i++) {
+    const chr = pattern[i]
+    if (patternFrequencyMap[chr] === undefined) {
+      patternFrequencyMap[chr] = 1
+    } else {
+      patternFrequencyMap[chr] += 1
+    }
+  }
+
+  
+  for (let winEnd = 0; winEnd < str.length; winEnd++) {
+    const chr = str[winEnd]
+
+    // If the character from input str matches a character in the HashMap, decrement its frequency in the map.
+    if (chr in patternFrequencyMap) {
+      patternFrequencyMap[chr] -= 1
+      
+      // If the character frequency becomes zero, we got a complete match.
+      if (patternFrequencyMap[chr] === 0) matched += 1
+    }
+
+    //If at any time, the number of characters matched is equal to the number of distinct characters in the pattern (i.e., total characters in the HashMap), we have gotten our required permutation.
+    if (matched === Object.keys(patternFrequencyMap).length) {
+      return true
+    }
+    // If the window size is greater than the length of the pattern,
+    // shrink the window to make it equal to the pattern’s size.
+    // At the same time, if the character going out was part of the pattern, put it back in the frequency HashMap.
+    if (winEnd > k) {
+      let leftChr = str[winStart]
+
+      winStart += 1
+      if (leftChr in patternFrequencyMap) {
+        if (patternFrequencyMap[leftChr] === 0) {
+          matched -= 1
+        }
+        patternFrequencyMap[leftChr] += 1
+      }
+    }
+  }
+  return false
 }
 
 
 let Input1 = "oidbcaf"
 let Pattern1 = "abc"
 
-const res1 = findPermutation(Input1) //Output: true
+const res1 = findPermutation(Input1, Pattern1) //Output: true
 
 console.log('res1 ', res1)
