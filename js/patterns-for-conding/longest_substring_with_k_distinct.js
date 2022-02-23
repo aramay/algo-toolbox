2e527429ae4fc24d0c61a8afcb8179816bcd3d8c
aramay <kasper_341@hotmail.com>
2022-02-22 19:25:49 -0800

    fix formatting

--- js/patterns-for-conding/longest_substring_with_k_distinct.js
+++ js/patterns-for-conding/longest_substring_with_k_distinct.js
@@ -30,11 +30,11 @@ const longestSubstringWithKDistinct = (arr1, k) => {
 
   let maxLen = 0
 
-  for (windowEnd ; windowEnd < arr1.length; windowEnd++) {
+  for (windowEnd; windowEnd < arr1.length; windowEnd++) {
     console.log('inside for loop')
     let temp = arr1[windowEnd]
     // check if key exists
-    if(charCount[temp] === undefined) {
+    if (charCount[temp] === undefined) {
       charCount[temp] = 1
     } else {
       charCount[temp] += 1
@@ -43,13 +43,13 @@ const longestSubstringWithKDistinct = (arr1, k) => {
     //maxSubStr += 1
 
     while (Object.keys(charCount).length > k) {
-      console.log('charcount length' , Object.keys(charCount))
+      console.log('charcount length', Object.keys(charCount))
       console.log('inside while loop')
       // maxSubStr -= 1
       let temp = arr1[windowStart]
       // decrement repeating charcount
       charCount[temp] -= 1
-      if(charCount[temp] === 0) delete charCount[temp]
+      if (charCount[temp] === 0) delete charCount[temp]
       // increment windowStart
       windowStart += 1
 
@@ -62,16 +62,16 @@ const longestSubstringWithKDistinct = (arr1, k) => {
   return maxLen
 }
 
-let arr1 ="araaci"
-let K=2
+let arr1 = "araaci"
+let K = 2
 
 const res1 = longestSubstringWithKDistinct(arr1, K) //Output: 4
 
 console.log('res1 ', res1)
 
 
-let Input1 ="araaci" 
-let K1=1
+let Input1 = "araaci"
+let K1 = 1
 
 const res2 = longestSubstringWithKDistinct(Input1, K1) // Output: 2
-console.log( res2 === 2)
+console.log(res2 === 2)
