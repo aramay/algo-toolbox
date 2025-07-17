1753c9e53c3c98d85d81ae5a3b09f6abff2b1d97
aramay <kasper_341@hotmail.com>
2025-07-17 08:15:05 -0700

    Add sum_optimized solution for perfectNumbers

--- js_ts/perfectNumber.ts
+++ js_ts/perfectNumber.ts
@@ -2,6 +2,39 @@
  * https://web.stanford.edu/class/archive/cs/cs106b/cs106b.1252/assignments/1-cpp/perfect#observing-the-runtime
  */
 
+import { result } from "cypress/types/lodash";
+
+// The actual code's optimized loop
+function sum_optimized(num){
+    let sum_optimized = 1;
+    for (let i = 2; i * i <= num; i++) { // Loop goes up to 5 (since 5*5=25 <= 28, but 6*6=36 > 28)
+    if (num % i === 0) {
+        sum_optimized += i;
+        if (i !== num / i) {
+            sum_optimized += num / i;
+
+            }
+        }
+    }
+    return sum_optimized === num
+}
+
+const t = sum_optimized(28)
+console.log(t)
+
+
+// For 28:
+// i=2: 28 % 2 === 0
+//    sum_optimized += 2 (sum=3)
+//    2 !== 28/2 (2 !== 14) -> true. sum_optimized += 14 (sum=17)
+// i=3: 28 % 3 !== 0
+// i=4: 28 % 4 === 0
+//    sum_optimized += 4 (sum=21)
+//    4 !== 28/4 (4 !== 7) -> true. sum_optimized += 7 (sum=28)
+// i=5: 28 % 5 !== 0
+// Loop ends because 6 * 6 = 36, which is not <= 28.
+// Final sum_optimized = 28
+
 // Hypothetical non-optimized loop for finding divisors
 function sum_non_optimized(num) {
   let sum_non_optimized = 1;
@@ -16,8 +49,8 @@ function sum_non_optimized(num) {
   console.log("final sum ", sum_non_optimized, "\n");
 }
 
-sum_non_optimized(28);
-sum_non_optimized(36);
+// sum_non_optimized(28);
+// sum_non_optimized(36);
 // For 28:
 // i=2, sum += 2 (sum=3)
 // i=3, no
