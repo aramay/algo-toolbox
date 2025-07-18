000382ef5e6e37f4e1b7d48be1783ae5ca0b1d83
aramay <kasper_341@hotmail.com>
2025-07-18 09:43:26 -0700

    Add solutions for validAnagram

--- js_ts/ValidAnagrams.js
+++ js_ts/ValidAnagrams.js
@@ -4,6 +4,32 @@
  * https://neetcode.io/problems/is-anagram
  */
 
+
+const isAnagram = (s, t) => {
+  if(s.length !== t.length){
+    return false;
+  }
+  let count = new Array(26).fill(0)
+  console.log(count)
+  for(let i =0; i <s.length; i++){
+    let temp = s.charCodeAt(i) - 97
+    
+    count[s.charCodeAt(i)-97]++;
+    count[t.charCodeAt(i)-97]--;
+  }
+for(let i =0; i < 26; i++){
+    if(count[i] !== 0){
+        return false;
+    }
+
+}
+
+return true;
+};
+
+// Test Cases:
+console.log(isAnagram("listen", "silent"));     // true
+
 /**
  * 242. Valid Anagram
 Easy
@@ -11,8 +37,6 @@ Topics
 premium lock icon
 Companies
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
- 
 
 Example 1:
 
@@ -26,8 +50,6 @@ Input: s = "rat", t = "car"
 
 Output: false
 
- 
-
 Constraints:
 
 1 <= s.length, t.length <= 5 * 104
@@ -38,15 +60,49 @@ STRETCH-CHALLENGE : Follow up: What if the inputs contain Unicode characters? Ho
  */
 const validAnagram = (s, t) => {
     console.log("validanagram")
+    const S = new Map()
+    const T = new Map()
+
+    const len = s.length
+
+    if (s.length !== t.length) return false
+
+    for (let i = 0; i < len; i++) {
+        let tempS = s[i]
+        let tempT = t[i]
+        if (!S.has(tempS)) {
+            S.set(tempS, 1)
+        } else {
+            S.set(tempS, S.get(tempS) + 1)
+        }
+        if (!T.has(tempT)) {
+            T.set(tempT, 1)
+        } else {
+            T.set(tempT, T.get(tempT) + 1)
+        }
+    }
+
+    for (let [key, ] of S.entries()) {
+
+        if (S.get(key) !== T.get(key)) {
+            return false
+        }
+    }
+    console.log(S)
+    console.log(T)
+    return true
+
 }
 // Example 1:
 let s = "anagram", t = "nagaram"
 // Output: true
-const testcase1 = validAnagram(s, t)
-console.log(testcase1)
+// const testcase1 = validAnagram(s, t)
+// console.log(testcase1)
 
 // Example 2:
 
-let s = "rat", t = "car"
+let s1 = "rat", t1 = "car"
 
 // Output: false
+const testcase2 = validAnagram(s1, t1)
+console.log(testcase2)
