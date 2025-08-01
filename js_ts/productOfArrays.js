0236725f3d41bce9bfa1abf7c612f5a4cec92085
aramay <kasper_341@hotmail.com>
2025-08-01 08:01:18 -0700

    add file productOfArrays

---
+++ js_ts/productOfArrays.js
@@ -0,0 +1,29 @@
+/**
+ * Neetcode: https://neetcode.io/problems/products-of-array-discluding-self?list=neetcode150
+ * Products of Array Except Self
+Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+
+Each product is guaranteed to fit in a 32-bit integer.
+
+Follow-up: Could you solve it in 
+O
+(
+n
+)
+O(n) time without using the division operation?
+
+Example 1:
+
+Input: nums = [1,2,4,6]
+
+Output: [48,24,12,8]
+Example 2:
+
+Input: nums = [-1,0,1,2,3]
+
+Output: [0,-6,0,0,0]
+Constraints:
+
+2 <= nums.length <= 1000
+-20 <= nums[i] <= 20
+ */
