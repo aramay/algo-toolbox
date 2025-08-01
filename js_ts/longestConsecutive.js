6d34862e9dda104d26f5890a3191331cac65d113
aramay <kasper_341@hotmail.com>
2025-08-01 11:30:50 -0700

    Add file longestConsecutive

---
+++ js_ts/longestConsecutive.js
@@ -0,0 +1,32 @@
+/**
+ * Neetcode: https://neetcode.io/problems/longest-consecutive-sequence?list=neetcode150
+ * 
+ * Longest Consecutive Sequence
+Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+
+A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. The elements do not have to be consecutive in the original array.
+
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+
+Input: nums = [2,20,4,10,3,4,5]
+
+Output: 4
+Explanation: The longest consecutive sequence is [2, 3, 4, 5].
+
+Example 2:
+
+Input: nums = [0,3,2,5,4,6,1,1]
+
+Output: 7
+Constraints:
+
+0 <= nums.length <= 1000
+-10^9 <= nums[i] <= 10^9
+ */
+
+const longestConsecutive = (nums) => {
+    console.log(nums)
+}
+
