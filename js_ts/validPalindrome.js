5c083cfc8c816ecc4648018effc9f64275f354f4
aramay <kasper_341@hotmail.com>
2025-08-08 12:33:35 -0700

    add problem description

---
+++ js_ts/validPalindrome.js
@@ -0,0 +1,28 @@
+/**
+ * Neetcode: https://neetcode.io/problems/is-palindrome?list=neetcode150
+ * Valid Palindrome
+Given a string s, return true if it is a palindrome, otherwise return false.
+
+A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+
+Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+
+Example 1:
+
+Input: s = "Was it a car or a cat I saw?"
+
+Output: true
+Explanation: After considering only alphanumerical characters we have "wasitacaroracatisaw", which is a palindrome.
+
+Example 2:
+
+Input: s = "tab a cat"
+
+Output: false
+Explanation: "tabacat" is not a palindrome.
+
+Constraints:
+
+1 <= s.length <= 1000
+s is made up of only printable ASCII characters.
+ */
